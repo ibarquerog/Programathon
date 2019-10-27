6221e0bea02930a85a3229e46de97b049366bb4f
ibarquerog <nacho.bg17@gmail.com>
2019-10-27 04:41:39 -0600

    Arreglando la torta

--- Fundacion_DEHVI/app/src/main/java/com/example/fundacion_dehvi/MyListAdapter.java
+++ Fundacion_DEHVI/app/src/main/java/com/example/fundacion_dehvi/MyListAdapter.java
@@ -31,55 +31,78 @@ import java.util.Date;
 import Concretos.Estudiante;
 
 
-public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder>{
+public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder> {
     private ArrayList<Estudiante> listdata;
-    private ArrayList<String> aq3=new ArrayList<String>();
+    private ArrayList<String> aq3 = new ArrayList<String>();
 
     // RecyclerView recyclerView;
     public MyListAdapter(ArrayList<Estudiante> listdata) throws ParseException {
         this.listdata = listdata;
         calculateMonths();
     }
+
     private void calculateMonths() throws ParseException {
-        for(Estudiante es:listdata) {
+        for (Estudiante es : listdata) {
             Date current = new Date();
             String cDate = new SimpleDateFormat("yyyy/MM/dd").format(current);
-            String dob=es.getFormattedDate();
+            String dob = es.getFormattedDate();
             SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
             Date a = format.parse(cDate);
-            Date b=format.parse(dob);
-            long dif=getMonthDifference(a,b);
+            Date b = format.parse(dob);
+            long dif = getMonthDifference(a, b);
             aq3.add(getAQ3(dif));
         }
     }
-    private String getAQ3(long dif){
-        if(dif>=1 && dif<3){ return "2 Meses ASQ-3";}
-        else if(dif>=3 && dif<5){ return "4 Meses ASQ-3";}
-        else if(dif>=5 && dif<7){ return "6 Meses ASQ-3";}
-        else if(dif>=7 && dif<9){ return "8 Meses ASQ-3";}
-        else if(dif>=9 && dif<10){ return "9 Meses ASQ-3";}
-        else if(dif>=10 && dif<11){ return "10 Meses ASQ-3";}
-        else if(dif>=11 && dif<13){ return "12 Meses ASQ-3";}
-        else if(dif>=13 && dif<15){ return "14 Meses ASQ-3";}
-        else if(dif>=15 && dif<17){ return "16 Meses ASQ-3";}
-        else if(dif>=17 && dif<19){ return "18 Meses ASQ-3";}
-        else if(dif>=19 && dif<21){ return "20 Meses ASQ-3";}
-        else if(dif>=21 && dif<23){ return "22 Meses ASQ-3";}
-        else if(dif>=23 && dif<25){ return "24 Meses ASQ-3";}
-        else if(dif>=25 && dif<28){ return "27 Meses ASQ-3";}
-        else if(dif>=28 && dif<31){ return "30 Meses ASQ-3";}
-        else if(dif>=31 && dif<34){ return "33 Meses ASQ-3";}
-        else if(dif>=34 && dif<39){ return "36 Meses ASQ-3";}
-        else if(dif>=39 && dif<45){ return "42 Meses ASQ-3";}
-        else if(dif>=45 && dif<51){ return "48 Meses ASQ-3";}
-        else if(dif>=51 && dif<57){ return "54 Meses ASQ-3";}
-        else if(dif>=57 && dif<66){ return "30 Meses ASQ-3";}
-        else {
+
+    private String getAQ3(long dif) {
+        if (dif >= 1 && dif < 3) {
+            return "2 Meses ASQ-3";
+        } else if (dif >= 3 && dif < 5) {
+            return "4 Meses ASQ-3";
+        } else if (dif >= 5 && dif < 7) {
+            return "6 Meses ASQ-3";
+        } else if (dif >= 7 && dif < 9) {
+            return "8 Meses ASQ-3";
+        } else if (dif >= 9 && dif < 10) {
+            return "9 Meses ASQ-3";
+        } else if (dif >= 10 && dif < 11) {
+            return "10 Meses ASQ-3";
+        } else if (dif >= 11 && dif < 13) {
+            return "12 Meses ASQ-3";
+        } else if (dif >= 13 && dif < 15) {
+            return "14 Meses ASQ-3";
+        } else if (dif >= 15 && dif < 17) {
+            return "16 Meses ASQ-3";
+        } else if (dif >= 17 && dif < 19) {
+            return "18 Meses ASQ-3";
+        } else if (dif >= 19 && dif < 21) {
+            return "20 Meses ASQ-3";
+        } else if (dif >= 21 && dif < 23) {
+            return "22 Meses ASQ-3";
+        } else if (dif >= 23 && dif < 25) {
+            return "24 Meses ASQ-3";
+        } else if (dif >= 25 && dif < 28) {
+            return "27 Meses ASQ-3";
+        } else if (dif >= 28 && dif < 31) {
+            return "30 Meses ASQ-3";
+        } else if (dif >= 31 && dif < 34) {
+            return "33 Meses ASQ-3";
+        } else if (dif >= 34 && dif < 39) {
+            return "36 Meses ASQ-3";
+        } else if (dif >= 39 && dif < 45) {
+            return "42 Meses ASQ-3";
+        } else if (dif >= 45 && dif < 51) {
+            return "48 Meses ASQ-3";
+        } else if (dif >= 51 && dif < 57) {
+            return "54 Meses ASQ-3";
+        } else if (dif >= 57 && dif < 66) {
+            return "30 Meses ASQ-3";
+        } else {
             return "";
         }
     }
 
-    private long getMonthDifference(Date a, Date b){
+    private long getMonthDifference(Date a, Date b) {
         long different = a.getTime() - b.getTime();
         long secondsInMilli = 1000;
         long minutesInMilli = secondsInMilli * 60;
@@ -90,10 +113,11 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
         return elapsedMonths;
 
     }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View listItem= layoutInflater.inflate(R.layout.recycler_view_item, parent, false);
+        View listItem = layoutInflater.inflate(R.layout.recycler_view_item, parent, false);
         ViewHolder viewHolder = new ViewHolder(listItem);
         return viewHolder;
     }
@@ -101,12 +125,12 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         final Estudiante estudiante = listdata.get(position);
-        holder.textView.setText(listdata.get(position).getFirstName()+" "+listdata.get(position).getLastName());
+        holder.textView.setText(listdata.get(position).getFirstName() + " " + listdata.get(position).getLastName());
         holder.asq3.setText(aq3.get(position));
         holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(view.getContext(),"click on item: "+estudiante.getFirstName(),Toast.LENGTH_LONG).show();
+                Toast.makeText(view.getContext(), "click on item: " + estudiante.getFirstName(), Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -121,13 +145,15 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
         public LinearLayout relativeLayout;
         public TextView asq3;
         public ImageView line;
+
         public ViewHolder(View itemView) {
             super(itemView);
-            this.asq3=(TextView) itemView.findViewById(R.id.recycler_view_item_ASQ3);
+            this.asq3 = (TextView) itemView.findViewById(R.id.recycler_view_item_ASQ3);
             this.textView = (TextView) itemView.findViewById(R.id.recycler_view_item_full_name);
-            this.line=(ImageView) itemView.findViewById(R.id.line);
+            this.line = (ImageView) itemView.findViewById(R.id.line);
             relativeLayout = (LinearLayout) itemView.findViewById(R.id.relativelayout);
         }
 
 
+    }
 }
