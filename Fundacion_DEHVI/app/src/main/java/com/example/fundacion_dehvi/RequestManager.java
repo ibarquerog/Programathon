14bec5fda143bd216553243fe4926c19dae12c5e
Pvillab <paulvillabeita@gmail.com>
2019-10-27 04:17:38 -0600

    Update RequestManager.java
    .

--- Fundacion_DEHVI/app/src/main/java/com/example/fundacion_dehvi/RequestManager.java
+++ Fundacion_DEHVI/app/src/main/java/com/example/fundacion_dehvi/RequestManager.java
@@ -15,10 +15,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -270,6 +269,37 @@ public class RequestManager {
             public void onErrorResponse(VolleyError error) {
                 //calificarASQ3Activity.onResponseGetAreas(null);
                 Log.d("dddddd", "Error: " + error.getMessage());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + LoginData.accessToken);
+                return headers;
+            }
+        };
+        queue.add(req);
+    }
+
+    public void requestAddResults(JSONObject json){
+        String requestString = this.connectionString.concat("/Result/AddResults");
+        RequestQueue queue = Volley.newRequestQueue(current);
+
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, requestString,
+                json, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Response", response.toString());
+
+                Toast.makeText(getCurrent(), "" + response.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error", "Error: " + error.getMessage());
+                Toast.makeText(getCurrent(), "" + error.getMessage(), Toast.LENGTH_SHORT).show();
+
             }
         }) {
             @Override
